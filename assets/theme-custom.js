bb5fb96654197c6ab27cda129fdad3dfcd47e811
kamrul0424 <kamrulhasanshuvo04@gmail.com>
2024-10-02 04:27:47 +0600

    product slider

---
+++ assets/theme-custom.js
@@ -0,0 +1,24 @@
+const swiper = new Swiper(".swiper.slideproducts", {
+  direction: "horizontal",
+  loop: true,
+  slidesPerView: 4,
+  spaceBetween: 10,
+  breakpoints: {
+    320: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    480: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+    640: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+  },
+  navigation: {
+    nextEl: ".ps-button-next",
+    prevEl: ".ps-button-prev",
+  },
+});
